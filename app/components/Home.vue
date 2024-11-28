a1dcc3315f2af776e43b3c063fdb91ccd34164bf
mthdht <mthdht@gmail.com>
2024-11-28 09:42:08 +0100

    add image placeholder with baby infos(name, sexe, age)

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -2,17 +2,29 @@
   <Page>
     <ActionBar>
       <Label text="BABY" class="font-bold text-lg" />
+      <ActionItem
+        ios.systemIcon="9" ios.position="left"
+        android.systemIcon="ic_menu_share" android.position="actionBar" />
     </ActionBar>
 
-    <ListView height="100%" for="item in flicks">
-      <v-template>
-        <Label :text="item.title" />
-      </v-template>
-    </ListView>
+    <StackLayout class="bg-red-100 p-4">
+      <!-- Conteneur pour l'image et les informations superposées -->
+      <AbsoluteLayout rows="auto" columns="*" class="relative w-full h-[800px]">
+        <!-- Image du bébé -->
+        <Image src="~/assets/baby-photo.jpg" class="absolute w-full h-full rounded-lg object-cover bg-yellow-300" />
+        <!-- Overlay -->
+        <FlexboxLayout top="400" class="w-full" alignItems="center">
+          <Label text="Alexendria" class="text-2xl font-bold pl-6" flexGrow="1"/>
+          <Label text="6 mois" class="text-lg font-semibold px-4" />
+          <Label text="7.5 kg" class="text-lg font-semibold pr-6" />
+        </FlexboxLayout>
+      </AbsoluteLayout>
+    </StackLayout>
   </Page>
 </template>
 
 <script lang="ts">
+import { AbsoluteLayout, FlexboxLayout, Image, StackLayout } from '@nativescript/core';
 import Vue from 'nativescript-vue';
 
 export default Vue.extend({
@@ -30,8 +42,6 @@ export default Vue.extend({
 });
 </script>
 
-<style>
-/* .info {
-    font-size: 20;
-  } */
+<style scoped>
+
 </style>
